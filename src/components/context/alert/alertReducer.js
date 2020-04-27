598876091fa139c8bfd34e172a17b547fd8e1b23
joaquin lema <joaco.lema@hotmail.com>
2020-04-27 20:03:36 -0300

    agregarmos REDUCER PARA alert

---
+++ src/components/context/alert/alertReducer.js
@@ -0,0 +1,23 @@
+import {
+SET_ALERT,
+REMOVE_ALERT
+} from '../types';
+
+// recibimos el initial state de la app y las action representan lo que pasamos por parametro en el dispatch
+export default (state,action) => {
+    // ...state lo que hace es hacer una copia exacta del state de la app para no modificar la original
+    // y poder cambiarle los valores que queremos
+    switch(action.type){
+        case SET_ALERT:
+            return{
+                ...state,
+            }
+        case REMOVE_ALERT:
+            return{
+                ...state,
+            }
+
+        default :
+        return state;
+    }
+}

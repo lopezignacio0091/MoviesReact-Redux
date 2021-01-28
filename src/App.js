0747b7e4d3ec90f60c22950e4c880e13239520a0
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-01-28 14:47:11 -0300

    agregamos navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import WeatherBox from './components/layout/WeatherBox';
+import WeatherAppBar from './components/layout/appBar/AppBar';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -22,6 +23,7 @@ const App = () => {
 
   return (
     <Provider store={store}>
+      <WeatherAppBar />
       <div className={classes.root}>
         <Grid container spacing={3}>
           <Grid item xs={12}>

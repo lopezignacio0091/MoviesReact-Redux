f35b2e6ca1bb70802937394ad28fd3c92e2a25f8
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-01-28 13:36:05 -0300

    agregamos el weather box a la home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import Store from './store';
+import WeatherBox from './components/layout/WeatherBox';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
 
   return (
     <Provider store={Store}>
-      <div>
-        
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <WeatherBox />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     </Provider>
   );
--- src/components/layout/WeatherBox.js
+++ src/components/layout/WeatherBox.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {useEffect} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {getWeather} from '../../actions/WeatherActions';
 import WeatherList from './WeatherList';
 
 const WeatherBox = ({weatherReducer: { loading, weatherData}}) => {
+
+    useEffect(() => {
+        getWeather();
+    }, [])
+
 
     if(loading) return <h2>cargando ...</h2>
     return (

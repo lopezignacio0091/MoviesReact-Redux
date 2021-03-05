a45f4acdc1e0a0c04d95d0a9fd0aef831477fa29
Juan Ignacio Lopez <juani.lopez@softtek.com>
2021-03-05 17:46:52 -0300

    Updates

---
+++ precache-manifest.fe1ed67f549e4b4d59c3abdc2d54a6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b334e10a8679ad011e82ce89854504bf",
+    "url": "/MoviesReact-Redux/index.html"
+  },
+  {
+    "revision": "dbc175b43a0a476b49e1",
+    "url": "/MoviesReact-Redux/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8327673c0232b72ce40a",
+    "url": "/MoviesReact-Redux/static/js/2.3e28167e.chunk.js"
+  },
+  {
+    "revision": "b315737083144f4c14b16a4a43df1485",
+    "url": "/MoviesReact-Redux/static/js/2.3e28167e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc175b43a0a476b49e1",
+    "url": "/MoviesReact-Redux/static/js/main.773f0b19.chunk.js"
+  },
+  {
+    "revision": "6b94285044501590c799",
+    "url": "/MoviesReact-Redux/static/js/runtime-main.bb3d3615.js"
+  }
+]);
